2e0621ec6fd523eb99439ef4ae5795791b90a2d9
Samuel Jansem <samuel.jansem@gmail.com>
2024-01-04 13:32:45 -0300

    Bump package version to 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="inter_pixpy",
-    version="0.0.2",
+    version="0.0.3",
     description="A Python package to interact with the Banco Inter's Pix API",
     author="Samuel Jansem",
     author_email="samuel.jansem@gmail.com",
